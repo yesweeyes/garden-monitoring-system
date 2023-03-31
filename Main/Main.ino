2daa6d7af1b90b152b7ca7a702d0ad7ef1cafdff
Shysu <shyamsundarv00073@gmail.com>
2023-03-31 19:56:32 +0530

    Main: Add soil moisture module function

--- Main/Main.ino
+++ Main/Main.ino
@@ -6,12 +6,24 @@ float temperatureSensorReading(int temperatureSensor_pin) {
   float valTempAnalog;
   float valTemp_Celsius;
 
-  valTempAnalog = analogRead(temperatureSensor_pin);
+  valTempAnalog = analogRead(this->temperatureSensor_pin);
   valTemp_Celsius = (valTempAnalog/1024)*5;
   valTemp_Celsius = valTemp_Celsius * 10;
 
   return valtemp_Celsius;
 }
+
+
+float soilMoistureSensorReading(int soilMoistureSensor_pin) {
+  float valMoistureAnalog;
+  float valMoisturePercenatge;
+
+  valMoistureAnalog = analogRead(this->soilMoistureSensor_pin);
+  valMoisturePercenatge = 100 - (valMoistureAnalog/1024)*100;
+
+  return valMoisturePercenatge;
+}
+
 
 void setup() {
   Serial.begin(9600)
