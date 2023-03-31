ae0a92943a0aa4f6528b6d1edc778b0a55effc7b
Shysu <shyamsundarv00073@gmail.com>
2023-03-31 19:53:18 +0530

    Main: Add temperature module function

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,18 @@
+const int temperatureSensor_pin = A3;
+const int soilMoistureSensor_pin = A4;
+const int lightDependentResistor_pin = A5;
+
+float temperatureSensorReading(int temperatureSensor_pin) {
+  float valTempAnalog;
+  float valTemp_Celsius;
+
+  valTempAnalog = analogRead(temperatureSensor_pin);
+  valTemp_Celsius = (valTempAnalog/1024)*5;
+  valTemp_Celsius = valTemp_Celsius * 10;
+
+  return valtemp_Celsius;
+}
+
 void setup() {
   Serial.begin(9600)
 
