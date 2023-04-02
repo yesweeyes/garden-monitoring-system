9297e3cbee3aea5692498b6b51be4533c0cbb0e1
Shysu <shyamsundarv00073@gmail.com>
2023-04-02 13:17:20 +0530

    Add humidity sensor module

--- HumiditySensor/HumiditySensor.ino
+++ HumiditySensor/HumiditySensor.ino
@@ -1,9 +1,20 @@
-const int humiditySensor_pin = 7;
+const int humiditySensor_pin = 4;
+
+int valHumidityAnalog;
+int valHumidityPercentage;
 
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(4, INPUT);
 }
 
 void loop() {
+  valHumidityAnalog = digitalRead(humiditySensor_pin);
+  valHumidityPercentage = map(valHumidityAnalog, 0, 1023, 10, 70);
 
+  Serial.print("Humidity: ");
+  Serial.print(valHumidityPercentage);
+  Serial.println("%");
+
+  delay(1000);
 }
